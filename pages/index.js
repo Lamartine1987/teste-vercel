d0676f15bbcdc10f791b89f7e007896854cdb9fa
Lamartine1987 <lamartinecezar@hotmail.com>
2021-12-05 10:40:45 -0300

    primeira página

---
+++ pages/index.js
@@ -0,0 +1,27 @@
+import {useState} from 'react';
+
+function Home(){
+    return (
+        <div>
+            <h1>Home</h1>
+            <Contador />
+        </div>
+    )
+}
+
+function Contador(){
+    const [contador, setcontador] = useState(1);
+
+    function adiconarContador(){
+        setcontador(contador +1);
+    }
+
+    return(
+        <div>
+            <div>{contador}</div>
+            <button onClick={adiconarContador} >Adicionar</button>
+        </div>
+    )
+}
+
+export default Home
